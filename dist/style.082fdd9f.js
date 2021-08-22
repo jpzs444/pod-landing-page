56f01d1b897aa5925a89fba91209c1076f77b14c
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2021-08-22 21:05:17 +0800

    feat: add initial background styles

--- dist/style.082fdd9f.js
+++ dist/style.082fdd9f.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\josep\\github\\pod-landing-page\\assets\\desktop\\image-host.jpg":[["image-host.43d2a2de.jpg","assets/desktop/image-host.jpg"],"assets/desktop/image-host.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
